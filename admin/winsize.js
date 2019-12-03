9e5425fbb9f72f66ee4eecb53d9e798a9a8e88cd
Ken Cowles <krcowles29@gmail.com>
2019-12-02 18:12:21 -0700

    removed additional unused files

--- admin/winsize.js
+++
@@ -1,5 +0,0 @@
-var divbox = document.getElementById('box');
-var rect = divbox.getBoundingClientRect();
-var ht = parseInt(rect.height);
-var wd = parseInt(rect.width);
-alert("Height by width: " + ht + " x " + wd);
