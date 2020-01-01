cbd40b76c2e01bd7135acd730aaef3f270e6f90e
Ken Cowles <krcowles29@gmail.com>
2020-01-01 08:11:09 -0700

    bug fix in month array indices

--- utilities/timeSetup.php
+++ utilities/timeSetup.php
@@ -13,14 +13,15 @@ $digits = explode("/", $date);
 $tbldate = $digits[2] . '-' . $digits[0] . '-' . $digits[1];
 $month_names = array('January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December');
-$thismo = intval($digits[0]) - 1; // array index is zero-based
+// array index is zero-based
+$thismo = intval($digits[0]) - 1;
 $current_month = $month_names[$thismo];
 switch ($thismo) {
-case 1:
-    $month_set = array(11, 12, 1);
+case 0:
+    $month_set = array(10, 11, 0);
     break;
-case 2:
-    $month_set = array(12, 1, 2);
+case 1:
+    $month_set = array(11, 0, 1);
     break;
 default:
     $month_set = array($thismo-2, $thismo-1, $thismo);
@@ -38,7 +39,7 @@ for ($i=0; $i<3; $i++) {
     $month[$i] = $month_names[$month_set[$i]];
 }
 $rollover = false;
-$LCM = "SELECT `LCM` FROM `Users` WHERE `user` = :uid;";
+$LCM = "SELECT `LCM` FROM `Users` WHERE `username` = :uid;";
 $moupdte = $pdo->prepare($LCM);
 $moupdte->execute(["uid" => $user]);
 $mo = $moupdte->fetch(PDO::FETCH_ASSOC);
