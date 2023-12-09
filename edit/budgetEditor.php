c700894413a7ba8c61050501b0623d3897004da5
Ken Cowles <krcowles29@gmail.com>
2023-12-09 16:50:37 -0700

    added 'total allocation' figure on budget manager to track total when changes are being made

--- edit/budgetEditor.php
+++ edit/budgetEditor.php
@@ -10,6 +10,8 @@
 session_start();
 require_once "../utilities/getAccountData.php";
 require_once "../utilities/timeSetup.php";
+$old_bud = array_sum($budgets);
+$old_bud = "$" . number_format($old_bud, 0, ".", ",");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,31 +39,34 @@ require_once "../utilities/timeSetup.php";
 <h4>You can add, delete, or rename accounts separately 
     using the menu: <em>Budget Mgr</em></h4>
 <form id="form" action="saveBudgetEdits.php" method="post">
-<div>
-    <button id="save" class="btn btn-secondary" type="button">
-            Save Edits</button>
-    <br /><br />
-    <table>
-        <thead>
+    <div>
+        <button id="save" class="btn btn-secondary" type="button">
+                Save Edits</button>
+        <br /><br />
+        <table id="bud_mgr">
+            <thead>
+                <tr>
+                    <th>Budget Item</th>
+                    <th>Monthly Budget</th>
+                    <th>Current Balance</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php for ($j=0; $j<count($account_names); $j++) : ?>
             <tr>
-                <th>Budget Item</th>
-                <th>Monthly Budget</th>
-                <th>Current Balance</th>
+                <td><?= $account_names[$j];?></td>
+                <td ><textarea class="monitor" rows="1" cols="8"
+                    name="edbud[]"><?=$budgets[$j];?></textarea></td>
+                <td><textarea rows="1" cols="14"
+                    name="edcurr[]"><?=$current[$j];?></textarea></td>
             </tr>
-        </thead>
-        <tbody>
-        <?php for ($j=0; $j<count($account_names); $j++) : ?>
-        <tr>
-            <td><?= $account_names[$j];?></td>
-            <td><textarea rows="1" cols="8"
-                name="edbud[]"><?=$budgets[$j];?></textarea></td>
-            <td><textarea rows="1" cols="14"
-                name="edcurr[]"><?=$current[$j];?></textarea></td>
-        </tr>
-        <?php endfor; ?>
-        </tbody>
-    </table><br />
-</div>
+            <?php endfor; ?>
+            </tbody>
+        </table>
+        <div id="budsum"><span>Total Monthly Allocatio: </span>
+            <span id="allocation"><?=$old_bud;?></span><br />
+        </div>
+    </div>
 </form>
 </div>
 <?php require "../main/bootstrapModals.html"; ?>
@@ -71,10 +76,34 @@ require_once "../utilities/timeSetup.php";
 <script src="../scripts/jquery.min.js"></script>
 <script src="../scripts/menus.js"></script>
 <script>
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0
+    });
     $('#edbuds').addClass('active');
     $('#save').on('click', function() {
         $('form').trigger('submit');
     });
+    var tbl_pos = $('#bud_mgr').offset();
+    var twidth = $('#bud_mgr').width() + 60;
+    $('#budsum').css({
+        top: tbl_pos.top + 40,
+        left: twidth
+    });
+    $('.monitor').each(function() {
+            $(this).on('change', function(ev) {
+                var curbud = parseInt($(this).text());
+                var newbud = parseInt(ev.target.value);
+                var delta  = newbud - curbud;
+                var cursum = $('#allocation').text().substr(1);
+                var cursum_cnvt = parseInt(cursum.replace(/[^\d\.]/, ''));
+                var newsum = cursum_cnvt + delta;
+                var sumtxt = formatter.format(newsum)
+                $('#allocation').html(sumtxt);
+            });
+        }
+    );
 </script>
 
 </body>
